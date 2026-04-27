3a5d725bfabb684e247f1775fa8b387a6516dbe5
Solis3D <127130916+Solis3D@users.noreply.github.com>
2026-04-27 15:21:43 +0200

    Aggiunge l'enitity Cliente e l'enum TipoCliente

---
+++ src/main/java/BuildWeek/Group3/entities/Cliente.java
@@ -0,0 +1,64 @@
+package BuildWeek.Group3.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "clienti")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Cliente {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_cliente")
+    private UUID idCliente;
+
+    @Column(name = "ragione_sociale", nullable = false)
+    private String ragioneSociale;
+
+    @Column(name = "partita_iva", nullable = false, unique = true)
+    private String partitaIva;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "data_inserimento", nullable = false)
+    private LocalDate dataInserimento;
+
+    @Column(name = "data_ultimo_contatto")
+    private LocalDate dataUltimoContatto;
+
+    @Column (name = "fatturato_annuale")
+    private double fatturatoAnnuale;
+
+    private String pec;
+
+    private String telefono;
+
+    @Column(name = "email_contatto")
+    private String emailContatto;
+
+    @Column(name = "nome_contatto")
+    private String nomeContatto;
+
+    @Column(name = "cognome_contatto")
+    private String cognomeContatto;
+
+    @Column(name = "telefono_contatto")
+    private String telefonoContatto;
+
+    @Column(name = "logo_aziendale")
+    private String logoAziendale;
+
+    @Enumerated(EnumType.STRING)
+    @Column (name = "tipo_cliente", nullable = false)
+    private TipoCliente tipoCliente;
+}
---
+++ src/main/java/BuildWeek/Group3/entities/TipoCliente.java
@@ -0,0 +1,5 @@
+package BuildWeek.Group3.entities;
+
+public enum TipoCliente {
+    PA, SAS, SPA, SRL
+}
