d5d6f4d3486a2b92aba040b2ec2493596573d8e8
SergioMoschini <moschini2002@gmail.com>
2026-04-28 10:47:42 +0200

    .

--- src/main/java/BuildWeek/Group3/repositories/IndirizzoRepository.java
+++ src/main/java/BuildWeek/Group3/repositories/IndirizzoRepository.java
@@ -10,6 +10,6 @@ public interface IndirizzoRepository extends JpaRepository<Indirizzo, UUID> {
     List<Indirizzo> findByCliente_IdCliente(UUID idCliente);
     List<Indirizzo> findByComune_IdComune(UUID idComune);
     List<Indirizzo> findByTipoIndirizzo(String tipoIndirizzo);
-    List<Indirizzo> findByCliente_IdClienteAndTipoIndirizzo(UUID idCliente, String tipoIndirizzo);
-    List<Indirizzo> findByCliente_IdClienteAndComune_IdComune(UUID idCliente, UUID idComune);
+    List<Indirizzo> findByClienteIdClienteAndTipoIndirizzo(UUID idCliente, String tipoIndirizzo);
+    List<Indirizzo> findByClienteIdClienteAndComune_IdComune(UUID idCliente, UUID idComune);
 }
